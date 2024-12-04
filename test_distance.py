4747ce1441dbb838b65ef4f5b7e1a10da4e2487c
Renee Cai <rcai3@jh.edu>
2024-12-04 12:19:25 -0500

    testing

--- test_distance.py
+++ test_distance.py
@@ -5,5 +5,5 @@ kmer_1 = ['AG', 'GC']
 kmer_2 = ['GC', 'CT']
 pytrie_1 = PatriciaTrie(kmer_1)
 pytrie_2 = PatriciaTrie(kmer_2)
-dist_a = mash_dist(pytrie_1, pytrie_2, 2, jaccard_sim_a = True)
-print(dist_a)
+j, dist_a = mash_dist(pytrie_1, pytrie_2, 2, jaccard_sim_a = True)
+print(j, dist_a)
