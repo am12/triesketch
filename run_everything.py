6a88cbe7d105cf90169de506ce9c692830ef9090
Renee Cai <rcai3@jh.edu>
2024-12-05 18:43:53 -0500

    edrfg

--- run_everything.py
+++ run_everything.py
@@ -6,10 +6,10 @@ from triesketch import create_phylo_tree
 def main():
     names = ["MZ169912", "MZ219592", "MW913395", "LC623948", "MW981442", "MZ202314", "MW789246", "MZ277392"]
     #read in files
-    genome_files = [f'./triesketch/correct_data_sets/{names[i]}.fasta' for i in range(0,8)] #change this for actual data datasets/LC623948.txt
+    genome_files = [f'./correct_data_sets/{names[i]}.fasta' for i in range(0,8)] #change this for actual data datasets/LC623948.txt
     final_dict = []
     
-    kmer_length = 21
+    kmer_length = 40
     
     #this adds the tries to the final_dict
     for index, genome_file in enumerate(genome_files):
@@ -21,7 +21,7 @@ def main():
         trie = PatriciaTrie(keys=kmers)
         final_dict.append(trie)
     
-    k_values = [13] #can change to the values we want to test or just change for loop to test range of k values
+    k_values = [31] #can change to the values we want to test or just change for loop to test range of k values
     
     distance_matrix_1 = np.zeros((len(names), len(names)))
     distance_matrix_2 = np.zeros((len(names), len(names)))
